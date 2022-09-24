53d741bd7a5a16dd0facb442bf35b0a4850e53aa
estudio315 <suscripciones@315.cl>
2022-09-24 13:39:45 -0300

    se agrego la pagina

---
+++ js/script.js
@@ -0,0 +1,20 @@
+   
+   var swiper = new Swiper(".mySwiper", {
+        autoplay: {
+          delay: 5000,
+        },
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
+
+  
---
+++ js/typed.js
@@ -0,0 +1,13 @@
+const typed = new Typed('.typed', {
+       strings : [
+        '<b class="palabra">eficaz</b>',
+        '<b class="palabra">económica</b>', 
+        '<b class="palabra">rapida</b>'
+    
+    ],
+     
+    typeSpeed: 40,
+    startDelay: 300,
+    backSpeed: 75, 
+    loop:true
+});
